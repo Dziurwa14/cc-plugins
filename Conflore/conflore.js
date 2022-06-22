56051863c70a370b7ad5ca156d842b3f808b6c49
Papaj <54191536+Dziurwa14@users.noreply.github.com>
2022-06-22 18:07:26 +0200

    conflore plugin

---
+++ Conflore/conflore.js
@@ -0,0 +1,38 @@
+import { findByProps, findAll } from "@cumcord/modules/webpack"
+const { getChannelId } = findByProps("getChannelId", "getVoiceChannelId");
+const messageQueue = findAll(arg => arg.enqueue)[0]
+
+function enqueueMessage(text) {
+    return messageQueue.enqueue(
+        {
+            "type": 0,
+            "message": {
+                channelId: getChannelId(),
+                content: text
+            }
+        },
+        r => {
+            return;
+        }
+    )
+}
+
+// Ideally we would want to be able to remap the keys but i'm just here to make this functional and not shit
+function keyHandler(event) {
+    if(event.altKey == true && event.keyCode == 70) { // ALT + F - Dog
+        event.preventDefault();
+        return enqueueMessage("https://cdn.discordapp.com/attachments/854050545093181471/984887138241896498/y2mate.com_-__480p.mp4");
+    }
+    return event;
+}
+
+export default (data) => {
+    return {
+        onLoad() {
+            document.addEventListener("keydown", keyHandler)
+        },
+        onUnload() {
+            document.removeEventListener("keydown", keyHandler)
+        }
+    }
+}
