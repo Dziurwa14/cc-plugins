f6af492219868e45e976ea8d75882781d70531de
Papaj <54191536+Dziurwa14@users.noreply.github.com>
2022-06-22 18:13:11 +0200

    goofy ahhh

--- Conflore/conflore.js
+++ Conflore/conflore.js
@@ -19,7 +19,7 @@ function enqueueMessage(text) {
 
 // Ideally we would want to be able to remap the keys but i'm just here to make this functional and not shit
 function keyHandler(event) {
-    if(event.altKey == true && event.keyCode == 70) { // ALT + F - Dog
+    if(event.altKey == true && event.keyCode == 70) {
         event.preventDefault();
         return enqueueMessage("https://cdn.discordapp.com/attachments/854050545093181471/984887138241896498/y2mate.com_-__480p.mp4");
     }
