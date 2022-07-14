d68ffd68497feaaf6ef510bceefeea5887899c04
Papaj <54191536+Dziurwa14@users.noreply.github.com>
2022-07-14 14:18:35 +0200

    new plugin activities pog

---
+++ Activities/main.js
@@ -0,0 +1,107 @@
+import { findByProps } from '@cumcord/modules/webpack';
+import { instead } from '@cumcord/patcher'
+import { dirtyDispatch } from '@cumcord/modules/common/FluxDispatcher'
+
+export default () => {
+  let Activities
+  return {
+    onLoad() {
+      Activities = instead('fetchShelf', findByProps("fetchShelf"), () => {
+        return dirtyDispatch({
+             type: "EMBEDDED_ACTIVITY_FETCH_SHELF_SUCCESS",
+             guildId: findByProps("getLastSelectedGuildId").getLastSelectedGuildId(),
+             items: [
+              {
+                  "application_id": "880218394199220334",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 0
+              },
+              {
+                  "application_id": "945737671223947305",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "903769130790969345",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 0
+              },
+              {
+                  "application_id": "832025144389533716",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "832013003968348200",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "852509694341283871",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "902271654783242291",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "879863686565621790",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "832012774040141894",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "755827207812677713",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                  "application_id": "947957217959759964",
+                  "expires_on": null,
+                  "new_until": null,
+                  "nitro_requirement": false,
+                  "premium_tier_level": 1
+              },
+              {
+                "application_id": "976052223358406656",
+                "expires_on": null,
+                "new_until": null,
+                "nitro_requirement": false,
+                "premium_tier_level": 0
+              }
+          ],
+          expires_at: new Date(null)
+      })
+      })
+    },
+    onUnload() {
+      Activities()
+    }
+  }
+}
